71b9a7a46238c0b6a5552cc08010225504bd2787
Ray Mathew <ray10mathew@gmail.com>
2016-09-25 20:12:45 +0530

    Tidied up the file

--- Python/ChromeOpenSites.py
+++ Python/ChromeOpenSites.py
@@ -1,12 +1,16 @@
 import webbrowser
 
 #Add website URLs in double quotes at the end of the list below. Don't forget to add a comma (,) between URLs.
+
 sites = [
     "https://www.facebook.com/",
     "https://www.google.com/gmail/",
     "https://twitter.com/"
 ]
+
+
 #To use any other browser, give the complete path like below. Do not remove the '%s', and either use single forward slashes (/) or
 # double backward slashes (\\) to denote going into a file (In Windows).
+
 for s in sites:
     webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open(s)
